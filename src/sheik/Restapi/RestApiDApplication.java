1d8c92ba09f9b2d56181de95dc483430488b2514
sheikhameed17 <108709759+sheikhameed17@users.noreply.github.com>
2023-08-25 15:22:08 +0530

    Add files via upload

---
+++ src/sheik/Restapi/RestApiDApplication.java
@@ -0,0 +1,13 @@
+package com.sheik.Restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiDApplication.class, args);
+	}
+
+}
